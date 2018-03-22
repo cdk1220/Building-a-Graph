42a251e24685ac8fc989315809f305de6ae2562c
Don Chalanga Kuruppu <donchalangakuruppu@Dons-MacBook-Pro.local>
2018-03-22 18:41:54 -0500

    Successfully instantiated prefab using the script.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public Transform pointPrefab;
+
+	void Awake () {
+		Instantiate(pointPrefab);
 	}
 }
