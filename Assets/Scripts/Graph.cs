36b4c30d0f12cac8b97c5de8a252e7ddacd7dc56
Don Chalanga Kuruppu <donchalangakuruppu@Dons-MacBook-Pro.local>
2018-03-22 20:32:43 -0500

    Changing the number instantiated without changing the range (-1, 1) implemented.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -10,22 +10,30 @@ public class Graph : MonoBehaviour {
 	public int resolution = 15;		   //Number instantiated
 
 	void Awake () {
+
+		//Make sure the resolution is odd so that the range(-1, 1) is fully
+		//covered with a cube at the origin
+		if ((resolution / 2) == 0) {
+			resolution = resolution + 1;
+		}
 		
+
 		//Scale of a cube
-		Vector3 scale = Vector3.one / 5f;		
+		float step = 2 / ((float)resolution - 1);
+		Vector3 scale = Vector3.one * step;		
 		
 		//Position of a cube 
 		Vector3 position;
 
 		//Constant as we dont deal with the z plane
 		position.z = 0;     
-		
+		Debug.Log(step);
 		//Visualizing f(x) = x
 		for (int i = 0; i < resolution; i++) {
 			Transform point = Instantiate(pointPrefab);
 			
 			//Position for f(x) = x
-			position.x = ((i / 5f) - 1f);
+			position.x = ((i * step)  - 1f);
 			position.y = position.x;
 			point.localPosition = position;
 
