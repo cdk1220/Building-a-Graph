aebd512a2ffed07ae206cefefdb2380eb753bee7
Don Chalanga Kuruppu <donchalangakuruppu@Dons-MacBook-Pro.local>
2018-03-22 19:35:03 -0500

    Successfully created f(x) = x.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -8,14 +8,26 @@ public class Graph : MonoBehaviour {
 
 	void Awake () {
 		
+		//Scale of a cube
+		Vector3 scale = Vector3.one / 5f;		
+		
+		//Position of a cube 
+		Vector3 position;
+
+		//Constant as we dont deal with the z plane
+		position.z = 0;     
+		
+		//Visualizing f(x) = x
 		for (int i = 0; i < 11; i++) {
 			Transform point = Instantiate(pointPrefab);
 			
-			//Cubes span a range of -1 to 1 on the axis
-			point.localPosition = Vector3.right * ((i / 5f) - 1f);
+			//Position for f(x) = x
+			position.x = ((i / 5f) - 1f);
+			position.y = position.x;
+			point.localPosition = position;
 
 			//Bringing back the cubes together so there is no overlap
-			point.localScale = Vector3.one / 5f;
+			point.localScale = scale;
 		}
 	}
 }
