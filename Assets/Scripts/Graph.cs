5120833d1d3c183690f5a48c2d26636cc445f2c4
Don Chalanga Kuruppu <donchalangakuruppu@Dons-MacBook-Pro.local>
2018-03-22 22:53:49 -0500

    Made the graph a sine wave that changes with time.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -9,6 +9,20 @@ public class Graph : MonoBehaviour {
 	[Range(15, 105)]
 	public int resolution = 15;		   //Number instantiated
 
+	//List to contain points created
+	List<Transform> points = new List<Transform>();
+
+	void Update() {
+		
+		for (int i = 0; i < resolution; i++) {
+			Transform point = points[i];
+			Vector3 position = point.localPosition;
+			position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+			point.localPosition = position;
+		}
+		
+	}
+	
 	void Awake () {
 
 		//Make sure the resolution is odd so that the range(-1, 1) is fully
@@ -25,8 +39,8 @@ public class Graph : MonoBehaviour {
 		Vector3 position;
 
 		//Constant as we dont deal with the z plane
+		position.y = 0;
 		position.z = 0;     
-		Debug.Log(step);
 		
 		//Visualizing f(x) = x
 		for (int i = 0; i < resolution; i++) {
@@ -34,7 +48,6 @@ public class Graph : MonoBehaviour {
 			
 			//Position for f(x) = x
 			position.x = ((i * step)  - 1f);
-			position.y = position.x;
 			point.localPosition = position;
 
 			//Bringing back the cubes together so there is no overlap
@@ -42,6 +55,9 @@ public class Graph : MonoBehaviour {
 
 			//Make the clones children
 			point.SetParent(transform, false);
+
+			//Add created cube to the list
+			points.Add(point);
 		}
 	}
 }
